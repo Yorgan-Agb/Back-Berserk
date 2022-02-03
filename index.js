10e0c82bb1a805bd4aa4f2ea780efaa7dd6f086a
MatthiasTeks <mathias.vimbert@gmail.com>
2022-02-03 23:46:58 +0100

    fix conflit

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require('express')
 const connection = require('./db-config')
+const cors = require('cors')
 const app = express()
 const port = 4242
+
+app.use(cors())
 
 
 app.get('/BerserkShop', (req, res) => {
@@ -24,8 +27,55 @@ app.get('/BerserkShop/filter/', (req, res) => {
             res.status(200).json(result)
         }
     })
-   
 })
+
+app.get('/BerserkShop/categories/:id', (req, res) => {
+    const categorieId = req.params.id;
+    connection.query(
+        'SELECT * FROM annonces WHERE categories_id = ?',
+        [categorieId],
+        (err, result) => {
+            if(err) {
+              console.error(err);
+              res.status(500).send('Error retrieving this categorie');
+            } else {
+                res.status(200).json(result);
+            }
+        }
+    )
+});
+
+app.get('/BerserkShop/etats/:id', (req, res) => {
+    const etatsId = req.params.id;
+    connection.query(
+        'SELECT * FROM annonces WHERE etats_id = ?',
+        [etatsId],
+        (err, result) => {
+            if(err) {
+                console.error(err);
+                res.status(500).send('Error retrieving this etats');
+            } else {
+                res.status(200).json(result);
+            }
+        }
+    )
+});
+
+app.get('/BerserkShop/dates/:id', (req, res) => {
+    const datesId = req.params.id;
+    connection.query(
+        'SELECT * FROM annonces WHERE dates_id = ?',
+        [datesId],
+        (err, result) => {
+            if(err) {
+                console.error(err);
+                res.status(500).send('Error retrieving this dates');
+            } else {
+                res.status(200).json(result);
+            }
+        }
+    )
+});
 
 app.post('/BerserkShop/annonce/post', (req, res)=> {
     res.send('ça marche igo')
@@ -40,7 +90,6 @@ app.post('/BerserkShop/annonce/post', (req, res)=> {
             }
         }
     )
-    
 })
 
 
